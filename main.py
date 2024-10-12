a1ec1345865d7a41732598566bdbbfb2f30958a7
superAI4003 <doriasenyz@gmail.com>
2024-10-12 18:19:38 -0500

    test1

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ app.include_router(auth.router, prefix="/auth12",tags=["Auth"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
