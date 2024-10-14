8219b0e84ece9121900975072829c1a63f5a6a2f
superAI4003 <doriasenyz@gmail.com>
2024-10-14 15:16:06 -0500

    update cors errors

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ app = FastAPI()
 
 # Set CORS
 origins = [
-    "http://localhost:3000", 
-    "http://161.35.15.35:3000",
-     "*" # Adjust this to match the domain of your frontend if it's not running on localhost:3000
+    "http://161.35.15.35:3000",  # Update this to match the domain of your frontend
+    "*"  # This allows all origins. Consider removing this if you want to restrict access.
 ]
 
 app.add_middleware(
