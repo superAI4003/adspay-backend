5c2bdc200fb2a64ce88bcdc56b415870d545fc4e
superAI4003 <doriasenyz@gmail.com>
2024-10-14 14:47:44 -0500

    update cors agian

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ app.include_router(auth.router, prefix="/auth",tags=["Auth"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
