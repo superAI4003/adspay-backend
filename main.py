72a082df1ed1a8a5a5bec53e6d615f07495aadc6
superAI4003 <doriasenyz@gmail.com>
2024-10-14 13:28:30 -0500

    update auth

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import auth
 from db.database import engine, Base
 
 app = FastAPI()
 
+# Set CORS
+origins = [
+    "http://localhost:3000", 
+     "*" # Adjust this to match the domain of your frontend if it's not running on localhost:3000
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Create database tables
 Base.metadata.create_all(bind=engine)
 
-app.include_router(auth.router, prefix="/auth",tags=["Auth12"])
+app.include_router(auth.router, prefix="/auth",tags=["Auth"])
 
 if __name__ == "__main__":
     import uvicorn
